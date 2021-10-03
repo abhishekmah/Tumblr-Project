432d0f09c75f23d10d4085ce5e2a801c058487cc
Abhishek <9528442373.as@gmail.com>
2021-10-04 01:01:53 +0530

    client-side and server-side

--- server-side/src/configs/db.js
+++ server-side/src/configs/db.js
@@ -3,12 +3,3 @@ const connect = mongoose.connect(`mongodb://127.0.0.1:27017/tumblr_data`);
 
 module.exports = connect;
 
-
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://abhishek:<password>@firstcluster.eouui.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
